d21cab915089513eeb25ef84923fc4e8ad2f2c6d
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2024-01-14 20:14:46 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -7,6 +7,7 @@ coord_mapping = {
     4: 'coord_1_0', 5: 'coord_1_1', 6: 'coord_1_2',
     7: 'coord_2_0', 8: 'coord_2_1', 9: 'coord_2_2'
 }
+
 def clear_screen():
     # For Windows
     if os.name == 'nt':
@@ -14,6 +15,7 @@ def clear_screen():
     # For Linux and macOS
     else:
         os.system('clear')
+
 class TicTacToeTable:
     def __init__(self, db_name='tictactoe.db'):
         self.db_name = db_name
@@ -59,10 +61,12 @@ class TicTacToeTable:
         query = f'UPDATE tictactoe SET {coord} = ?, player_turn = ? WHERE matchid = ?;'
         self.cursor.execute(query, (symbol, player, matchid))
         self.conn.commit()
+
     def end_match(self, matchid, winner_name):
         query = f'UPDATE tictactoe SET winner_name = ? WHERE matchid = ?;'
         self.cursor.execute(query, (winner_name, matchid))
         self.conn.commit()
+
     def get_board_state(self, matchid):
         query = f'SELECT * FROM tictactoe WHERE matchid = ?;'
         self.cursor.execute(query, (matchid,))
@@ -85,7 +89,6 @@ class TicTacToeTable:
             [(0, 0), (1, 1), (2, 2)],
             [(0, 2), (1, 1), (2, 0)]
         ]
-
         for line in lines_to_check:
             values = [board_state[f'coord_{i}_{j}'] for i, j in line]
             if all(value == board_state['symbol_player1'] for value in values):
@@ -110,120 +113,123 @@ class TicTacToeTable:
         else:
             print("No winners found in the leaderboard.")
 
-def print_colored(text, color):
+    def create_new_match(self):
+        match_id = random.randint(1000, 9999)
+        player1 = input("Enter Player 1 name: ")
+        player2 = input("Enter Player 2 name: ")
+        symbol_player1 = input(f"Enter the symbol for {player1}: ")
+        symbol_player2 = input(f"Enter the symbol for {player2}: ")
+        self.insert_new_match(match_id, player1, player2, symbol_player1, symbol_player2)
+        print(f"\nNew match created with ID: {match_id}")
+        return match_id
+
+    def open_all_matches(self):
+        self.cursor.execute('SELECT * FROM tictactoe ;', ())
+        existing_matches = self.cursor.fetchall()
+
+        if existing_matches:
+            print("\nAll Matches:")
+            print("{:<10} {:<20} {:<20} {:<15}".format("Match ID", "Player 1", "Player 2", "Match Status"))
+            print("-" * 65)
+            for match in existing_matches:
+                match_id, player1, player2, match_status = match[0], match[2], match[3], "Live" if match[15] is None else match[15] + " won"
+                print("{:<10} {:<20} {:<20} {:<15}".format(str(match_id), player1, player2, match_status))
+        else:
+            print("No matches found.")
+
+    def open_existing_match(self, match_id=None):
+        if match_id is None:
+            match_id = input("Enter the match ID to open: ")
+        self.cursor.execute('SELECT * FROM tictactoe WHERE matchid = ?;', (match_id,))
+        existing_match = self.cursor.fetchone()
+
+        if existing_match:
+            print("\nExisting Match Details:")
+
+            if existing_match[15] is not None:
+                print(f"\nMatch with ID {match_id} has already ended. {existing_match[15]} won!")
+                return
+            while True:
+                board_state = self.get_board_state(match_id)
+
+                self.fill_boxes(match_id, 'player1')
+                board_state = self.get_board_state(match_id)
+
+                winner = self.check_winner(board_state)
+                print("------- ------------------------------attempt p1-----------------------------------" + str(
+                    winner))
+                if winner:
+                    self.end_match(match_id, winner)
+                    print(f"\nCongratulations! {winner} wins!")
+                    break
+
+                self.fill_boxes(match_id, 'player2')
+                board_state = self.get_board_state(match_id)
+
+                winner = self.check_winner(board_state)
+                print(
+                    "-------------------------------------attempt p2-----------------------------------" + str(
+                        winner))
+
+                if winner:
+                    self.end_match(match_id, winner)
+                    print(f"\nCongratulations! {winner} wins!")
+                    break
+
+        else:
+            print(f"Match with ID {match_id} not found.")
+
+    def fill_boxes(self, match_id, player):
+        board_state = self.get_board_state(match_id)
+        print("\nCurrent Board State:")
+        while True:
+            print("\n\n")
+            self.print_board(board_state)
+            position = int(input(
+                f"\n{board_state[player]}'s turn \nenter the position (1-9)\n Enter you move : "))
+
+            if position < 1 or position > 9:
+                print("Invalid position. Please choose a number between 1 and 9.")
+                continue
+
+            coord = coord_mapping[position]
+            if board_state[coord] is not None:
+                print("Invalid move. The position is already filled. Try again.")
+            else:
+                break
+
+        symbol = board_state['symbol_player1'] if player == 'player1' else board_state['symbol_player2']
+        self.update_board(match_id, coord, player, symbol)
+
+    def print_colored(self, text, color):
         colors = {
             'red': '\033[91m',
             'green': '\033[92m',
             'blue': '\033[94m',
             'reset': '\033[0m'
         }
-        return colors[color]+text+colors['reset']
-def print_board(board_state):
-    def format_cell(value,num):
-        positions=[1,2,3,4,5,6,7,8,9]   
-        if value is None:
-            return str(positions[num])
-        elif value == board_state['symbol_player1']:
-            return print_colored(value, 'blue')
-        elif value == board_state['symbol_player2']:
-            return print_colored(value, 'green')
-        else:
-            return ""+positions[num]
-    print(f"{format_cell(board_state['coord_0_0'],0)} | {format_cell(board_state['coord_0_1'],1)} | {format_cell(board_state['coord_0_2'],2)}")
-    print("---------")
-    print(f"{format_cell(board_state['coord_1_0'],3)} | {format_cell(board_state['coord_1_1'],4)} | {format_cell(board_state['coord_1_2'],5)}")
-    print("---------")
-    print(f"{format_cell(board_state['coord_2_0'],6)} | {format_cell(board_state['coord_2_1'],7)} | {format_cell(board_state['coord_2_2'],8)}")
-    print("\nPlayer Symbols:")
-    print(f"Player 1 ({board_state['player1']}): {board_state['symbol_player1']}")
-    print(f"Player 2 ({board_state['player2']}): {board_state['symbol_player2']}")
+        return colors[color] + text + colors['reset']
 
-def create_new_match(table_instance):
-    match_id = random.randint(1000, 9999)
-    player1 = input("Enter Player 1 name: ")
-    player2 = input("Enter Player 2 name: ")
-    symbol_player1 = input(f"Enter the symbol for {player1}: ")
-    symbol_player2 = input(f"Enter the symbol for {player2}: ")
-    table_instance.insert_new_match(match_id, player1, player2, symbol_player1, symbol_player2)
-    print(f"\nNew match created with ID: {match_id}")
-    return match_id
+    def print_board(self, board_state):
+        def format_cell(value, num):
+            positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            if value is None:
+                return str(positions[num])
+            elif value == board_state['symbol_player1']:
+                return self.print_colored(value, 'blue')
+            elif value == board_state['symbol_player2']:
+                return self.print_colored(value, 'green')
+            else:
+                return "" + positions[num]
 
-def fill_boxes(table_instance, match_id, player):
-    board_state = table_instance.get_board_state(match_id)
-    print("\nCurrent Board State:")
-    while True:
-        print("\n\n")
-        print_board(board_state)
-        position = int(input(f"\n{board_state[player]}'s turn \nenter the position (1-9)\n0- Exit \n Enter you move : "))
-
-        if position < 1 or position > 9:
-            print("Invalid position. Please choose a number between 1 and 9.")
-            continue
-
-        coord = coord_mapping[position]
-        if board_state[coord] is not None:
-            print("Invalid move. The position is already filled. Try again.")
-        else:
-            break
-
-    symbol = board_state['symbol_player1'] if player == 'player1' else board_state['symbol_player2']
-    table_instance.update_board(match_id, coord, player, symbol)
-
-def open_all_matches(table_instance):
-    table_instance.cursor.execute('SELECT * FROM tictactoe ;', ())
-    existing_matches = table_instance.cursor.fetchall()
-
-    if existing_matches:
-        print("\nAll Matches:")
-        print("{:<10} {:<20} {:<20} {:<15}".format("Match ID", "Player 1", "Player 2", "Match Status"))
-        print("-" * 65)
-
-        for match in existing_matches:
-            match_id, player1, player2, match_status = match[0], match[2], match[3], "Live" if match[13] is None else match[13] + " won"
-            print("{:<10} {:<20} {:<20} {:<15}".format(str(match_id), player1, player2, match_status))
-
-    else:
-        print("No matches found.")
-
-def open_existing_match(table_instance , match_id = None):
-    if(match_id is None):
-        match_id = input("Enter the match ID to open: ")
-    table_instance.cursor.execute('SELECT * FROM tictactoe WHERE matchid = ?;', (match_id,))
-    existing_match = table_instance.cursor.fetchone()
-
-    if existing_match:
-        print("\nExisting Match Details:")
-        print(existing_match)
-
-        player1 = existing_match[2]
-        player2 = existing_match[3]
-        symbol_player1 = existing_match[4]
-        symbol_player2 = existing_match[5]
-
-        while True:
-            board_state = table_instance.get_board_state(match_id)
-
-            fill_boxes(table_instance, match_id, 'player1')
-
-            winner = table_instance.check_winner(board_state)
-            print("------- ------------------------------attempt p1-----------------------------------"+str(winner))
-            if winner:
-                print(f"\nCongratulations! {winner} wins!")
-                table_instance.end_match(match_id, winner)
-                break
-
-            fill_boxes(table_instance, match_id, 'player2')
-
-            winner = table_instance.check_winner(board_state)
-            print("-------------------------------------attempt p2-----------------------------------"+str(winner))
-
-            if winner:
-                print(f"\nCongratulations! {winner} wins!")
-                table_instance.end_match(match_id, winner)
-                break
-
-    else:
-        print(f"Match with ID {match_id} not found.")
+        print(f"{format_cell(board_state['coord_0_0'], 0)} | {format_cell(board_state['coord_0_1'], 1)} | {format_cell(board_state['coord_0_2'], 2)}")
+        print("---------")
+        print(f"{format_cell(board_state['coord_1_0'], 3)} | {format_cell(board_state['coord_1_1'], 4)} | {format_cell(board_state['coord_1_2'], 5)}")
+        print("---------")
+        print(f"{format_cell(board_state['coord_2_0'], 6)} | {format_cell(board_state['coord_2_1'], 7)} | {format_cell(board_state['coord_2_2'], 8)}")
+        print("\nPlayer Symbols:")
+        print(f"Player 1 ({board_state['player1']}): {board_state['symbol_player1']}")
+        print(f"Player 2 ({board_state['player2']}): {board_state['symbol_player2']}")
 
 if __name__ == "__main__":
     db_name = 'tictactoe.db'
@@ -231,7 +237,6 @@ if __name__ == "__main__":
     tictactoe_table.create_table()
 
     while True:
-
         print("\n1. Create New Match")
         print("2. Open Existing Match")
         print("3. Show All Matches")
@@ -239,17 +244,17 @@ if __name__ == "__main__":
         print("5. Exit")
 
         choice = input("Enter your choice : ")
-        if(choice in ['1','3','4','5']):
+        if choice in ['1', '3', '4', '5']:
             clear_screen()  # Clear the terminal screen
 
         if choice == '1':
-            match_id = create_new_match(tictactoe_table)
-            open_existing_match(tictactoe_table,match_id)
+            match_id = tictactoe_table.create_new_match()
+            tictactoe_table.open_existing_match(match_id)
 
         elif choice == '2':
-            open_existing_match(tictactoe_table)
+            tictactoe_table.open_existing_match()
         elif choice == '3':
-            open_all_matches(tictactoe_table)
+            tictactoe_table.open_all_matches()
 
         elif choice == '4':
             tictactoe_table.show_leaderboard()
